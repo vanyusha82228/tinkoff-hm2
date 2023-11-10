fbfd6e6369feabb05e841c9cee8d0b7a6623b375
Ivan <ivan00.02@mail.ru>
2023-11-10 13:17:36 +0300

    completed task3

--- src/main/java/edu/hw5/Task5.java
+++ src/main/java/edu/hw5/Task5.java
@@ -1,8 +1,7 @@
 package edu.hw5;
 
 public class Task5 {
-    private static final String NUMBER_OF_CAR_PATTERN = "[А, В, Е, К, М, Н, О, Р, С, Т, У, Х]"
-        + "\\d{3}[А, В, Е, К, М, Н, О, Р, С, Т, У, Х]{2}\\d{2,3}";
+    private static final String NUMBER_OF_CAR_PATTERN = "^[АВЕКМНОРСТУХ]{1}\\d{3}[АВЕКМНОРСТУХ]{2}\\d{2,3}$";
 
     private Task5() {
     }

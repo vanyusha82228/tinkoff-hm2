cec1f46a5fe16d95db70feeafa3348c77a01d3d8
Ivan <ivan00.02@mail.ru>
2023-11-15 13:02:45 +0300

    completed task1

--- src/main/java/edu/hm7/Task1.java
+++ src/main/java/edu/hm7/Task1.java
@@ -3,7 +3,7 @@ package edu.hm7;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Task1 implements Runnable {
-    private static AtomicInteger counter = new AtomicInteger(0);
+    private static AtomicInteger  counter = new AtomicInteger(0);
 
     @Override
     public void run() {

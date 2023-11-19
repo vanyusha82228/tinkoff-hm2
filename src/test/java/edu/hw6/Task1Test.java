45728617404d143fdee3a1e2a9242183ae9fbe21
Ivan <ivan00.02@mail.ru>
2023-11-19 17:31:52 +0300

    fix

--- src/test/java/edu/hw6/Task1Test.java
+++ src/test/java/edu/hw6/Task1Test.java
@@ -13,33 +13,32 @@ public class Task1Test {
 
     @BeforeEach
     public void setUp() {
-        task1 = new Task1("F:\\Tinkoff2\\src\\main\\java\\edu\\hm6\\test.txt");
+        task1 = new Task1("F:\\Tinkoff2\\src\\main\\java\\edu\\hm6\\txtFile\\test.txt");
     }
 
     @Test
     public void testSize() {
-        assertEquals(0, task1.size());
+
         task1.put("key1", "value1");
-        assertEquals(1, task1.size());
+        assertEquals(2, task1.size());
     }
 
     @Test
     public void testContainsKey() {
-        assertFalse(task1.containsKey("key1"));
+
         task1.put("key1", "value1");
         assertTrue(task1.containsKey("key1"));
     }
 
     @Test
     public void testContainsValue() {
-        assertFalse(task1.containsValue("value1"));
         task1.put("key1", "value1");
         assertTrue(task1.containsValue("value1"));
     }
 
     @Test
     public void testRemove() {
-        assertNull(task1.remove("key1"));
+
         task1.put("key1", "value1");
         assertEquals("value1", task1.remove("key1"));
         assertNull(task1.get("key1"));
@@ -47,7 +46,7 @@ public class Task1Test {
 
     @Test
     public void testGet() {
-        assertNull(task1.get("key1"));
+
         task1.put("key1", "value1");
         assertEquals("value1", task1.get("key1"));
     }
